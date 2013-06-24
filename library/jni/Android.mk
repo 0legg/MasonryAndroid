5fface4ab2cf9e3e2076229d0177a90f0856f396
Oleg Godovykh <ogodovykh@noveogroup.com>
2013-06-24 15:39:37 +0700

    Provided makefiles to build with NDK

---
+++ library/jni/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)                     	
+LOCAL_MODULE    := masonry
+LOCAL_SRC_FILES := Masonry/DiscreteRect.cpp \
+                   Masonry/MasonryCore.cpp
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+                    $(LOCAL_PATH)/Masonry
+LOCAL_CPPFLAGS	+= -std=c++11
+LOCAL_LDLIBS    := -lm -llog
+include $(BUILD_SHARED_LIBRARY)
---
+++ library/jni/Application.mk
@@ -0,0 +1,5 @@
+NDK_TOOLCHAIN_VERSION=4.7
+APP_STL := gnustl_static
+APP_CPPFLAGS += -fexceptions
+APP_CPPFLAGS += -DANDROID
+APP_OPTIM := release
